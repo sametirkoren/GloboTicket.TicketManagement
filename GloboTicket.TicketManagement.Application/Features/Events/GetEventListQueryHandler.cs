a545fe5b7ab05036b5ced6c934100484e07c954f
Samet İrkören <samet.irkoren@yr.com.tr>
2021-04-06 09:31:45 +0300

    Application Logic in the Request Handler

---
+++ GloboTicket.TicketManagement.Application/Features/Events/GetEventListQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events
+{
+    public class GetEventListQueryHandler : IRequestHandler<GetEventsListQuery, List<EventListVm>>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+
+        public GetEventListQueryHandler(IAsyncRepository<Event> eventRepository, IMapper mapper)
+        {
+            _eventRepository = eventRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<EventListVm>> Handle(GetEventsListQuery request, CancellationToken cancellationToken)
+        {
+            var allEvents = (await _eventRepository.ListAllAsync()).OrderBy(x => x.Date);
+            return _mapper.Map<List<EventListVm>>(allEvents);
+        }
+    }
+}
---
+++ GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Events;
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Event, EventListVm>().ReverseMap();
+            CreateMap<Event, EventDetailVm>().ReverseMap();
+            CreateMap<Category, CategoryDto>();
+        }
+    }
+}
