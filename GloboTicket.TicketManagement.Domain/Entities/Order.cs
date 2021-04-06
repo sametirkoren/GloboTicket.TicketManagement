044e7db327e6515af2d8fd07d5a1e567530e618a
Samet İrkören <samet.irkoren@yr.com.tr>
2021-04-06 08:47:42 +0300

    Add project files.

---
+++ GloboTicket.TicketManagement.Domain/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GloboTicket.TicketManagement.Domain.Common;
+
+namespace GloboTicket.TicketManagement.Domain.Entities
+{
+   public  class Order : AuditableEntity
+    {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+
+        public bool OrderPaid { get; set; }
+    }
+}
