cbda2ecda931be1c8b4835447f8eadb57aadde40
Samet İrkören <samet.irkoren@yr.com.tr>
2021-04-06 08:53:09 +0300

    Creating Application Project

---
+++ GloboTicket.TicketManagement.Application/Contracts/Persistence/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Contracts.Persistence
+{
+    public interface ICategoryRepository : IAsyncRepository<Category>
+    {
+    }
+}
